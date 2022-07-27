b7f0598d72e38cb2544c924e114743e458be9e7b
Edi Haryono <energipositif@gmail.com>
2022-07-28 00:22:23 +0700

    Create database.php

---
+++ codeigniter/postgre/database.php
@@ -0,0 +1,21 @@
+$db['postgre'] = array(
+	'dsn'	=> 'pgsql:host=localhost;port=5432;dbname=dikti;password=071002239',
+	'hostname' => 'localhost',
+	'username' => 'postgres',
+	'password' => '071002239',
+	'database' => 'dikti',
+	'dbdriver' => 'pdo',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
